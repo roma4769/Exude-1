9a666b0ec7ec1509c7097ddac11a182d20efd477
roma4769 <skubrievromario@gmail.com>
2021-07-08 12:41:16 +0300

    0.1
    nicknames, yayeban -> exude

--- js/yess.js
+++ js/yess.js
@@ -12,7 +12,7 @@ function loaded() {
 	setInterval(loop, 400);
 }
 var x = 0;
-var titleText = ["|", "R|", "RO|", "ROM|", "ROMA|", "ROMA4|" ,"ROMA47|","ROMA476|","ROMA4769|","ROMA476|","ROMA47|","ROMA4|","ROMA|","ROM|","RO|","R|","|"];
+var titleText = ["|", "E|", "EX|", "EXU|", "EXUD|", "EXUDE|" ,"EXUD|","EXU|","EX|","E|","|"];
 
 function loop() {
 	document.getElementsByTagName("title")[0].innerHTML = titleText[x++ % titleText.length];
